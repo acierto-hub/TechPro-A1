719d64f9ce1cbc8d5afcb9aaa1f0de9e97d1ef64
acierto-hub <angeloyooriacierto.scc@gmail.com>
2025-08-22 01:44:59 +0000

    Done

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String [] args) {
-System.out.println("Hello World");
+System.out.println("Acierto Angelo Yoori");
+int countdown = 3;
+while (countdown > 0){
+    System.out.println(countdown);
+    countdown --;
+}
+System.out.println("Happy New Year");
     }
 }
