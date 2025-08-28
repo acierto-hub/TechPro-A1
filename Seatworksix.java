0d2f60e06c3ed6802cbd08422b1098e477e4ece4
acierto-hub <angeloyooriacierto.scc@gmail.com>
2025-08-28 09:41:36 +0000

    Seatworksix

--- Seatworksix.java
+++ Seatworksix.java
@@ -3,10 +3,10 @@ public class Seatworksix {
         		System.out.println    ("Seatworksix");
                 		int [] [] lotteryCard = { {20, 15, 7}, {8, 7, 19}, {7, 13, 47} 
                         		
-                                		};
+					};
                                         		
-                                                		int [] [] lotteryCard2= new int [3] [3];
-                                                        		
+                           		int [] [] lotteryCard2= new int [3] [3];
+                                                     		
                                                                 		lotteryCard2 [0][0] = 20;
                                                                         		lotteryCard2 [0][1] = 15;
                                                                                 		lotteryCard2 [0][2] = 7;
