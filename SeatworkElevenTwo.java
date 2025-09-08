8939f4f4f58effcef5fe8d704795f66c5ebaea8a
acierto-hub <angeloyooriacierto.scc@gmail.com>
2025-09-08 01:57:18 +0000

    done

---
+++ SeatworkElevenTwo.java
@@ -0,0 +1,9 @@
+public class SeatworkElevenTwo {
+    public static void main(String[] args) {
+        		System.out.println("SEATWORKELEVENPARTTWO");
+                		System.out.println();
+                        		int[][] myNumbers ={{1, 4, 2},{3,6,8} };
+                                		myNumbers[1][2] = 9;
+                                        		System.out.println(myNumbers[1][2]);
+    }
+}
